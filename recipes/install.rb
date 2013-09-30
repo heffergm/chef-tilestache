c924e530f841b0f38ddc425e19b3cd285b0ba07b
Grant Heffernan <heffergm@gmail.com>
2013-09-30 09:05:58 -0400

    add

---
+++ recipes/install.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: tilestache
+# Recipe:: install
+#
+# Copyright 2013, Mapzen
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'tilestache::service'
+include_recipe 'tilestache::gunicorn'
+
+python_pip 'tilestache' do
+  action :install
+  version "#{node[:tilestache][:tilestache][:version]}"
+end
+
+#bash 'tilestache_setup_py' do
+#  action :nothing
+#  user 'root'
+#  group 'root'
+#  cwd "#{node[:tilestache][:install_path]}"
+#  code <<-EOH
+#    #{node[:python][:binary]} setup.py install
+#  EOH
+#end
+
+#git "#{node[:tilestache][:install_path]}" do
+#  action :sync
+#  repository "#{node[:tilestache][:repository]}"
+#  revision   "#{node[:tilestache][:revision]}"
+#  user       "#{node[:tilestache][:user]}"
+#  group      "#{node[:tilestache][:group]}"
+#  notifies :restart, 'service[tilestache]', :delayed
+#  notifies :run, 'bash[tilestache_setup_py]', :immediately
+#end
+
+include_recipe 'tilestache::apache'
